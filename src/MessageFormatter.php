cd395a01873dafb5cf5679d111ce27c06fd18cb2
Craig Duncan <git@duncanc.co.uk>
2017-02-08 16:10:02 +0000

    Create a message formatter to include the relevant request/response info

---
+++ src/MessageFormatter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace duncan3dc\Guzzle;
+
+use GuzzleHttp\Psr7;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class MessageFormatter extends \GuzzleHttp\MessageFormatter
+{
+    public function format(RequestInterface $request, ResponseInterface $response = null, \Exception $error = null)
+    {
+        $message = str_repeat("<", 80) . "\n";
+
+        $message .= trim(Psr7\str($request)) . "\n";
+
+        if ($response) {
+            $message .= "--------------------------------------------------------------------------------\n";
+            $message .= trim(Psr7\str($response)) . "\n";
+        }
+
+        $message .= str_repeat(">", 80) . "\n";
+
+        return $message;
+    }
+}
---
+++ tests/MessageFormatterTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace duncan3dc\GuzzleTests;
+
+use duncan3dc\Guzzle\MessageFormatter;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+
+class MessageFormatterTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testFormatRequest()
+    {
+        $message = new MessageFormatter;
+
+        $request = new Request("POST", "http://example.com/", [
+            "Header1"   =>  "One",
+        ], "body");
+
+        $expected = [
+            "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+            "POST / HTTP/1.1",
+            "Host: example.com",
+            "Header1: One",
+            "",
+            "body",
+            ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
+            "",
+        ];
+
+        $result = $message->format($request);
+        $lines = preg_split("/\r?\n/", $result);
+
+        $this->assertSame($expected, $lines);
+    }
+
+
+    public function testFormatRequestAndResponse()
+    {
+        $message = new MessageFormatter;
+
+        $request = new Request("POST", "http://example.com/", [
+            "Header1"   =>  "One",
+        ], "body");
+
+        $response = new Response(404, [
+            "Format"    =>  "JSON",
+        ], "no-text");
+
+        $expected = [
+            "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+            "POST / HTTP/1.1",
+            "Host: example.com",
+            "Header1: One",
+            "",
+            "body",
+            "--------------------------------------------------------------------------------",
+            "HTTP/1.1 404 Not Found",
+            "Format: JSON",
+            "",
+            "no-text",
+            ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
+            "",
+        ];
+
+        $result = $message->format($request, $response);
+        $lines = preg_split("/\r?\n/", $result);
+
+        $this->assertSame($expected, $lines);
+    }
+}
